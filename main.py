36be82ded8795a785fcf65c783879b025d45000d
J0seCamp0s <jgabriel.campos.u@gmail.com>
2024-03-10 16:22:05 -0600

    Add get_command and remove_space methods to menu class, and basic menu loop.

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 class menu():
     def __init__(self, command):
         self.command = command
+
+    @staticmethod
+    def get_command(usr_inpt):
+        menu.remove_space(usr_inpt)
+    
+    @staticmethod
+    def remove_space(inpt_str):
+        fixed_str = ""
+        for ch in inpt_str:
+            if ch != ' ':
+                fixed_str += ch
+        return (fixed_str)
+
+while True:
+    print("Please choose a type of file to read\n")
+    print("Type html for .html file\nType xml for .xml\nType custom for custom language file\n")
+    mode = input("Please enter your input: ")
