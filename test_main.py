da05ffe4d17b1f2decafa16ba5e7cc9dd6a0a52c
J0seCamp0s <jgabriel.campos.u@gmail.com>
2024-03-10 19:52:40 -0600

    Add tests for get_command method in main.py

--- test_main.py
+++ test_main.py
@@ -1,3 +1,18 @@
 import pytest
+from main import menu
 
+def test_get_command1():
+    output = menu.get_command(input: "XML")
+    assert output == 0
 
+def test_get_command2():
+    output = menu.get_command(input: "HtMl")
+    assert output == 1
+
+def test_get_command3():
+    output = menu.get_command(input: "C u    S   t  Om")
+    assert output == 2
+
+def test_get_command3():
+    output = menu.get_command(input: "notaccept")
+    assert output == -1
