450f6122100d02db5cf8b8431d8c04f97f435d4e
J0seCamp0s <jgabriel.campos.u@gmail.com>
2024-03-10 19:59:34 -0600

    Fix sytanx error in test_main.py

--- test_main.py
+++ test_main.py
@@ -2,15 +2,15 @@ import pytest
 from main import menu
 
 def test_get_command1():
-    output = menu.get_command(input: "XML")
+    output = menu.get_command("XML")
     assert output == 0
 
 def test_get_command2():
-    output = menu.get_command(input: "HtMl")
+    output = menu.get_command("HtMl")
     assert output == 1
 
 def test_get_command3():
-    output = menu.get_command(input: "C u    S   t  Om")
+    output = menu.get_command("C u    S   t  Om")
     assert output == 2
 
 def test_get_command3():
