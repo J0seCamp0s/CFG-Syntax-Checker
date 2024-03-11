4f97105ef05b193361ebcd4b3ac01eb8d06da849
J0seCamp0s <jgabriel.campos.u@gmail.com>
2024-03-11 13:15:57 -0600

    Add CFG class defnition and builder methods for it

---
+++ parser.py
@@ -0,0 +1,16 @@
+class CFG():
+    def __init__(self, nonterminals,terminals, productions,start,inpt):
+        self.nonterminals = nonterminals
+        self.terminals = terminals
+        self.productions = productions
+        self.start = start
+        self.inpt = inpt
+    @staticmethod
+    def html_build():
+        pass
+    @staticmethod
+    def xml_build():
+        pass
+    @staticmethod
+    def custom_build():
+        pass
