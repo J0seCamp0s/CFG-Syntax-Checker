a648829949a2877e583c2ee06b42b21b6b507f43
J0seCamp0s <jgabriel.campos.u@gmail.com>
2024-03-10 20:11:39 -0600

    Refactor test_main.py to not require manual inputs

--- test_main.py
+++ test_main.py
@@ -1,18 +1,15 @@
 import pytest
 from main import menu
 
-def test_get_command1():
-    output = menu.get_command("XML")
-    assert output == 0
+@pytest.mark.parametrize("input_str, expected_output", 
+[("XML", 0),("HtMl", 1),("C u    S   t  Om", 2),("notaccept", -1)])
 
-def test_get_command2():
-    output = menu.get_command("HtMl")
-    assert output == 1
-
-def test_get_command3():
-    output = menu.get_command("C u    S   t  Om")
-    assert output == 2
-
-def test_get_command3():
-    output = menu.get_command("notaccept")
-    assert output == -1
+def test_get_command(input_str, expected_output, monkeypatch):
+    # Mock input
+    monkeypatch.setattr('builtins.input', lambda _: input_str)
+    
+    # Call the function
+    output = menu.get_command()
+    
+    # Assert the result
+    assert output == expected_output
