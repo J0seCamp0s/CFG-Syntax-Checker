bdafe6c518f1bfe98f69681b5df77a947dbc522c
J0seCamp0s <jgabriel.campos.u@gmail.com>
2024-03-11 13:39:26 -0600

    Add code to html_build method in CFG class

--- parser.py
+++ parser.py
@@ -6,8 +6,23 @@ class CFG():
         self.start = start
         self.inpt = inpt
     @staticmethod
-    def html_build():
-        pass
+    def html_build(path):
+        # Open the file
+        file = open(path, 'r')
+        # Read the file
+        inpt_str = file.read()
+        html_terminals = [
+        "<head>","</head>",
+        "<body>","</body>",
+        "<title>","</title>",
+        "<h1>","</h1>",
+        "<p>","</p>",
+        "html","/html",
+        "<ul>","</ul>",
+        "<li>","</li>",
+        "<a>","</a>"]
+        # Close the file
+        file.close()
     @staticmethod
     def xml_build():
         pass
