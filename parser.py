23f2247c4a52205404ace8ec943e6c9500c7c600
J0seCamp0s <jgabriel.campos.u@gmail.com>
2024-03-11 16:07:45 -0600

    Add elements to html_terminals array

--- parser.py
+++ parser.py
@@ -34,8 +34,31 @@ class CFG():
         "anchor",
         "div"]
         html_terminals = [
-        "<html>","</html>",
-        "<head>","</head>"
+        "<html>", "</html>",
+        "<head>", "</head>",
+        "<title>", "</title>",
+        "<body>", "</body>",
+        "<h1>", "</h1>",
+        "<h2>", "</h2>",
+        "<h3>", "</h3>",
+        "<h4>", "</h4>",
+        "<h5>", "</h5>",
+        "<h6>", "</h6>",
+        "<p>", "</p>",
+        "<a>", "</a>",
+        "<img>", 
+        "<ul>", "</ul>",
+        "<ol>", "</ol>",
+        "<li>", "</li>",
+        "<div>", "</div>",
+        "<span>", "</span>",
+        "<br>",  
+        "<hr>",  
+        "<strong>", "</strong>",
+        "<em>", "</em>",
+        "<input>", 
+        "<form>", "</form>",
+        "<textarea>", "</textarea>"
         ]
         # Close the file
         file.close()
