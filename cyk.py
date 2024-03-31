895974195b5e933478ed3398d28a0484b53fb6e1
J0seCamp0s <jgabriel.campos.u@gmail.com>
2024-03-30 21:27:13 -0600

    Adapt code for reading CFG into a static method

--- cyk.py
+++ cyk.py
@@ -1,29 +1,8 @@
 import xml.etree.ElementTree as ET
 
-tree_iter = ET.iterparse('Html_CFG.jff', events=('start', 'end'))
-
-prod_rules = {'S':[]}
-left_arr = []
-right_arr = []
-
-currentElement = 'S'
-
-for event, element in tree_iter:
-    if event == 'start':
-        if element.tag == 'left':
-            if currentElement != element.text:
-                currentElement = element.text
-                prod_rules[currentElement] = []
-            left_arr.append(element.text)
-        elif element.tag == 'right':
-            prod_rules[currentElement].append(element.text)
-            # Process the element here (e.g., extract data, analyze structure, etc.)
-        element.clear()  # Clear the element from memory to conserve memory usage
-
 class CFG():
     @staticmethod
     def get_cfg(type):
-        
         if type == 0:
             tree_iter = ET.iterparse('Xml_CFG.jff', events=('start', 'end'))
         if type == 1:
@@ -57,7 +36,6 @@ class CFG():
     def initialize_table(input_string, grammar):
         n = len(input_string)
         table = [[set() for _ in range(n)] for _ in range(n)]
-        
         # Initialize the table with terminals derived directly from input symbols
         for i, symbol in enumerate(input_string):
             for nonterm in grammar:
@@ -90,25 +68,25 @@ class CFG():
             c1_row = start_row
             c1_col = start_col
             start_col = start_col +1
-            print(f"Current position of c1: [{c1_row}][{c1_col}]")
+            #print(f"Current position of c1: [{c1_row}][{c1_col}]")
             while table[start_row][start_col] == set():
                 start_row = start_row + 1
                 if start_row == n:
-                    return("Table empty\n")
+                    return()
             c2 = table[start_row][start_col]
             c2_row = start_row
             c2_col = start_col
-            print(f"Current position of c2: [{c2_row}][{c2_col}]")
+            #print(f"Current position of c2: [{c2_row}][{c2_col}]")
             for j in c1:
                     for k in c2:
                         for key in grammar.keys():
                             for prod in grammar[key]:
                                 look_string = j+k
                                 if look_string in prod:
-                                    print(f"Current j: {j}")
-                                    print(f"Current k: {k}")
-                                    print(f"Found match in: {key}")
-                                    print(f"Writting to: [{write_r + row_offset}][{write_c + col_offset}]\n")
+                                    #print(f"Current j: {j}")
+                                    #print(f"Current k: {k}")
+                                    #print(f"Found match in: {key}")
+                                    #print(f"Writting to: [{write_r + row_offset}][{write_c + col_offset}]\n")
                                     table[write_r + row_offset][write_c + col_offset].add(key)
             start_col = n-1
             if start_row == row_max-1:
@@ -117,18 +95,12 @@ class CFG():
                 write_c += 1
                 row_offset = 0
                 col_offset = 0
-                print("Diagonal finished\n")
+                #print("Diagonal finished\n")
             else:
                 row_offset += 1
                 col_offset += 1
-                print("Iteration finished\n")
+                #print("Iteration finished\n")
             if row_max == 1:
-                print("Parse finish\n")
+                #print("Parse finish\n")
                 break    
 
-
-start_symbol = 'S'
-input_string = '<!DOCTYPE html><html></html>'
-
-result = CFG.cyk_algorithm(prod_rules, start_symbol, input_string)
-print(result)  # Output: True
